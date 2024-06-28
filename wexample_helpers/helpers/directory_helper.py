b6c015389c1c763a0e30d2c5b84527d35dba1219
weeger <romain.weeger@gmail.com>
2024-06-28 16:33:25 +0200

    Upgrade

--- wexample_helpers/helpers/directory_helper.py
+++ wexample_helpers/helpers/directory_helper.py
@@ -1,7 +1,19 @@
 import os
 import shutil
+from typing import Any
+
+from wexample_helpers.const.types import AnyCallable
 
 
 def directory_remove_tree_if_exists(directory: str) -> None:
     if os.path.exists(directory):
         shutil.rmtree(directory)
+
+
+def directory_execute_inside(target_dir: str, callback: AnyCallable) -> Any:
+    original_dir = os.getcwd()
+    os.chdir(target_dir)
+    response = callback()
+    os.chdir(original_dir)
+
+    return response
