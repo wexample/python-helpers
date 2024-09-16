ef4e1e8ba1256908f4e399f45034228dde25578d
weeger <romain.weeger@gmail.com>
2024-09-16 11:28:02 +0200

    Process command request

--- wexample_helpers/helpers/string_helper.py
+++ wexample_helpers/helpers/string_helper.py
@@ -12,4 +12,4 @@ def string_to_snake_case(text: str) -> str:
     s1 = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", text)
     s2 = re.sub("([a-z])([A-Z])", r"\1_\2", s1).lower()
     s3 = re.sub("([0-9])([A-Z])", r"\1\2", s2)
-    return str(re.sub("\W+", "_", s3))
+    return str(re.sub(r"\W+", "_", s3))
