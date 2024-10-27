54fd6f5ce2a91db8977c49e7057ac70749fb00e4
weeger <romain.weeger@gmail.com>
2024-10-27 10:21:40 +0100

    Mixins

---
+++ wexample_helpers/classes/mixin/has_short_class_name_class_mixin.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+
+class HasShortClassNameClassMixin:
+    @classmethod
+    def get_class_name_suffix(cls) -> Optional[str]:
+        return None
+
+    @classmethod
+    def get_short_class_name(cls) -> str:
+        short_name = cls.__name__
+        suffix = cls.get_class_name_suffix()
+
+        if suffix is not None and short_name.endswith(suffix):
+            return short_name[:-len(suffix)]
+
+        return short_name
---
+++ wexample_helpers/classes/mixin/has_snake_short_class_name_class_mixin.py
@@ -0,0 +1,9 @@
+from wexample_helpers.classes.mixin.has_short_class_name_class_mixin import HasShortClassNameClassMixin
+
+
+class HasSnakeShortClassNameClassMixin(HasShortClassNameClassMixin):
+    @classmethod
+    def get_snake_short_class_name(cls) -> str:
+        from wexample_helpers.helpers.string_helper import string_to_snake_case
+
+        return string_to_snake_case(cls.get_short_class_name())
