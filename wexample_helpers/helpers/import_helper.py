b37cafd6c346d3abe304e672c91ff31c9c75f044
weeger <romain.weeger@gmail.com>
2024-10-27 12:20:45 +0100

    Import helper

---
+++ wexample_helpers/helpers/import_helper.py
@@ -0,0 +1,12 @@
+from typing import List, Any
+
+
+def import_dummy(classes: Any) -> None:
+    """
+    Fix this error, by importing classes for in a dummy method. These classes are actually not used anywhere :
+    pydantic.errors.PydanticUserError: `ClassName` is not fully defined; you should define `Kernel`,
+    then call `CountAdminCommand.model_rebuild()`.
+    Using this method allow to add missing imports in file, event not used in page, and prevent auto formatters
+    to consider import as useless.
+    """
+    pass
