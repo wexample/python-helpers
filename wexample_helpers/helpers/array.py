12a9a673e98f1cd6a98b62dcf16780a52753b00f
weeger <romain.weeger@gmail.com>
2025-01-09 11:26:58 +0100

    Fix append children and test adding a missing line

--- wexample_helpers/helpers/array.py
+++ wexample_helpers/helpers/array.py
@@ -1,4 +1,4 @@
-from typing import Any, List, Sequence
+from typing import Any, List, Sequence, Optional
 
 
 def array_replace_value(
@@ -18,3 +18,14 @@ def array_swap(two_items_array: List[Any], do_swap: bool) -> List[Any]:
 
 def array_unique(array: Sequence[Any]) -> List[Any]:
     return list(set(array))
+
+def array_dict_get_by(key: str, value: Any, list_of_dicts: List[dict]) -> Optional[dict]:
+    """
+    Retrieve a dictionary from a list of dictionaries based on a specific key-value pair.
+
+    :param key: The key to search for in the dictionaries.
+    :param value: The value to match for the specified key.
+    :param list_of_dicts: The list of dictionaries to search.
+    :return: The first dictionary that matches the key-value pair, or None if not found.
+    """
+    return next((d for d in list_of_dicts if d.get(key) == value), None)
