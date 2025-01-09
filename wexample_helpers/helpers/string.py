452c47ff600c00d24934acfe8159b067bc7b0e91
weeger <romain.weeger@gmail.com>
2025-01-09 10:55:56 +0100

    To helper

--- wexample_helpers/helpers/string.py
+++ wexample_helpers/helpers/string.py
@@ -1,4 +1,5 @@
 import re
+from typing import List
 
 
 def string_to_kebab_case(text: str) -> str:
@@ -13,3 +14,20 @@ def string_to_snake_case(text: str) -> str:
     s2 = re.sub("([a-z])([A-Z])", r"\1_\2", s1).lower()
     s3 = re.sub("([0-9])([A-Z])", r"\1\2", s2)
     return str(re.sub(r"\W+", "_", s3))
+
+
+def string_append_missing_lines(lines: List[str], content: str) -> str:
+    # Split the current content into lines
+    current_lines = content.splitlines()
+
+    # Determine the lines that need to be added
+    lines_to_add = [line for line in lines if line not in current_lines]
+
+    if lines_to_add:
+        # Ensure the content ends with a newline before appending new lines
+        if content and not content.endswith('\n'):
+            content += '\n'
+        # Add the missing lines
+        content += '\n'.join(lines_to_add) + '\n'
+
+    return content
