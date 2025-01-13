08ee0f3df7a180b194d28cd4617c2dd4be8c2e9e
weeger <romain.weeger@gmail.com>
2025-01-13 14:02:01 +0100

    Debug helper

--- wexample_helpers/helpers/debug.py
+++ wexample_helpers/helpers/debug.py
@@ -1,10 +1,23 @@
-def debug_trace() -> None:
-    import traceback
-    print(''.join(traceback.format_stack()))
+from typing import List, Optional
+
+
+def debug_trace(print_output: bool = True) -> Optional[List[str]]:
+    import inspect
+    stack = []
+
+    for frame in inspect.stack()[1:]:
+        stack.append(f"{frame.filename}:{frame.lineno}")
+
+    if print_output:
+        print('\n'.join(stack))
+        return None
+    return stack
+
 
 def debug_trace_and_die() -> None:
-    debug_trace()
-    exit()
+    result = debug_trace()
+    exit(1)
+
 
 def dd() -> None:
     debug_trace_and_die()
