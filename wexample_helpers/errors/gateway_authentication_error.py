c5f6281be493dcffebd442181ab0ef52800376ca
weeger <romain.weeger@gmail.com>
2025-01-20 10:10:43 +0100

    Move errors classes

--- wexample_helpers/errors/gateway_authentication_error.py
+++
@@ -1,6 +0,0 @@
-from wexample_helpers.errors.gateway_error import GatewayError
-
-
-class GatewayAuthenticationError(GatewayError):
-    """Raised when authentication to the API fails."""
-    pass
--- wexample_helpers/errors/gateway_connexion_error.py
+++
@@ -1,6 +0,0 @@
-from wexample_helpers.errors.gateway_error import GatewayError
-
-
-class GatewayConnectionError(GatewayError):
-    """Raised when connection to the API fails."""
-    pass
