7e5aafb0c436a0e69918127118d9e50798330e42
weeger <romain.weeger@gmail.com>
2025-08-19 09:23:13 +0200

    Dead code

--- wexample_helpers/helpers/debug.py
+++ wexample_helpers/helpers/debug.py
@@ -9,20 +9,17 @@ from wexample_helpers.helpers.trace import trace_print
 
 def debug_trace(
         path_style: DebugPathStyle = DebugPathStyle.FULL,
-        truncate_stack: int = 0,
         paths_map: Optional[dict] = None
 ) -> None:
     # Delegate to trace helpers that use the new exception/trace classes
     trace_print(
         path_style=path_style,
-        truncate_stack=truncate_stack,
         paths_map=paths_map,
     )
 
 
 def debug_trace_and_die(
         path_style: DebugPathStyle = DebugPathStyle.FULL,
-        truncate_stack: int = 0,
         paths_map: Optional[dict] = None,
         message: str = None
 ) -> None:
@@ -30,7 +27,6 @@ def debug_trace_and_die(
         print(message)
     debug_trace(
         path_style=path_style,
-        truncate_stack=truncate_stack,
         paths_map=paths_map,
     )
     exit(1)
