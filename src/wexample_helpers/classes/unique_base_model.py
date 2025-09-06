73c8f700392b8c228011b3eb6491d3fafbb36e46
weeger <romain.weeger@gmail.com>
2025-09-06 23:53:32 +0200

    De-pydanticification

--- src/wexample_helpers/classes/unique_base_model.py
+++ src/wexample_helpers/classes/unique_base_model.py
@@ -9,12 +9,4 @@ class UniqueBaseModel(BaseModel):
     """
 
     def __init_subclass__(cls, **kwargs) -> None:
-        from wexample_helpers.errors.multiple_base_model_inheritance_error import MultipleBaseModelInheritanceError
-        super().__init_subclass__(**kwargs)
-        # Count the number of direct base classes that are subclasses of BaseModel.
-        base_model_count = sum(
-            1 for base in cls.__bases__ if issubclass(base, BaseModel)
-        )
-        if base_model_count > 1:
-
-            raise MultipleBaseModelInheritanceError(cls)
+        pass
