d1ca36a7cee6bc6d9f265457eb4c2cd098e40f72
weeger <romain.weeger@gmail.com>
2025-09-07 13:41:46 +0200

    Python sorting

--- src/wexample_helpers/const/ansi.py
+++ src/wexample_helpers/const/ansi.py
@@ -2,5 +2,6 @@ from __future__ import annotations
 
 import re
 
-CSI_RE = re.compile(r"\x1B\[[0-?]*[ -/]*[@-~]")
+# filestate: python-constant-sort
 OSC8_RE = re.compile(r"\x1B\]8;;.*?\x1B\\")
+CSI_RE = re.compile(r"\x1B\[[0-?]*[ -/]*[@-~]")
