c3b50d3387e21f88f6a00ec1b4ae11853ebdc51d
weeger <romain.weeger@gmail.com>
2025-09-07 13:57:02 +0200

    Python sorting

--- src/wexample_helpers/const/types.py
+++ src/wexample_helpers/const/types.py
@@ -37,14 +37,14 @@ FileStringOrPath = Union[str, Path]
 
 # Version types
 # filestate: python-constant-sort
-UPGRADE_TYPE_MAJOR: str = "major"
-UPGRADE_TYPE_INTERMEDIATE: str = "intermediate"
-UPGRADE_TYPE_MINOR: str = "minor"
 UPGRADE_TYPE_ALPHA: str = "alpha"
 UPGRADE_TYPE_BETA: str = "beta"
 UPGRADE_TYPE_DEV: str = "dev"
-UPGRADE_TYPE_RC: str = "rc"
+UPGRADE_TYPE_INTERMEDIATE: str = "intermediate"
+UPGRADE_TYPE_MAJOR: str = "major"
+UPGRADE_TYPE_MINOR: str = "minor"
 UPGRADE_TYPE_NIGHTLY: str = "nightly"
+UPGRADE_TYPE_RC: str = "rc"
 UPGRADE_TYPE_SNAPSHOT: str = "snapshot"
 
 VERSION_PRE_BUILD_NUMBER: int = 0
