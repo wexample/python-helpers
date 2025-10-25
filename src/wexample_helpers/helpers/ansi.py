0989e1e68c247137330dceb94e119fd5fc6ab8fc
weeger <romain.weeger@gmail.com>
2025-10-25 20:04:11 +0200

    Rect

--- src/wexample_helpers/helpers/ansi.py
+++ src/wexample_helpers/helpers/ansi.py
@@ -48,6 +48,7 @@ def ansi_strip_osc(text: str) -> str:
 
 
 def ansi_truncate_visible(text: str, max_width: int) -> str:
+    from wcwidth import wcwidth
     from wcwidth import _wcw
 
     if max_width <= 0:
