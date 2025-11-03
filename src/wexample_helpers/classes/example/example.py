59bd25f0fc8e0eba74b3e239d833c2d7a9c9d2d2
weeger <romain.weeger@gmail.com>
2025-11-03 08:54:16 +0100

    Example executor filters

--- src/wexample_helpers/classes/example/example.py
+++ src/wexample_helpers/classes/example/example.py
@@ -1,8 +1,9 @@
 from wexample_helpers.decorator.base_class import base_class
 from wexample_helpers.mixin.with_path_mixin import WithPathMixin
+from wexample_helpers.service.registry import RegistrableType
 
 
 @base_class
-class Example(WithPathMixin):
+class Example(WithPathMixin, RegistrableType):
     def execute(self) -> None:
         pass
