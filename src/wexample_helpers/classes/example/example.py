d8486bd132583b91fa4ba2a3afefccfa7ee7f431
weeger <romain.weeger@gmail.com>
2025-11-03 09:23:05 +0100

    Interactive executor

--- src/wexample_helpers/classes/example/example.py
+++ src/wexample_helpers/classes/example/example.py
@@ -1,8 +1,14 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
 
 from wexample_helpers.classes.field import public_field
 from wexample_helpers.classes.base_class import BaseClass
 from wexample_helpers.decorator.base_class import base_class
 from wexample_helpers.mixin.with_path_mixin import WithPathMixin
+
+if TYPE_CHECKING:
+    from wexample_helpers.classes.example.executor import Executor
 
 
 @base_class
