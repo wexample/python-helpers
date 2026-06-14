767677ffea31fc77bce367943ecf18d31e72a1f8
wexample_pip_suite_manager-performance agent <agent.wexample_pip_suite_manager-performance@wex.local>
2026-06-14 21:37:22 +0200

    [suite:wexample_pip_suite_manager/performance] use type() builtin over .__class__ lookup; set __isabstract__ on wrapper directly instead of mutating func

--- src/wexample_helpers/classes/abstract_method.py
+++ src/wexample_helpers/classes/abstract_method.py
@@ -4,11 +4,10 @@ import functools
 
 
 def abstract_method(func):
-    func.__isabstract__ = True
-
     @functools.wraps(func)
     def wrapper(*args, **kwargs) -> None:
-        cls = args[0].__class__.__name__
+        cls = type(args[0]).__name__
         raise NotImplementedError(f"{cls}.{func.__name__}() must be implemented")
 
+    wrapper.__isabstract__ = True
     return wrapper
